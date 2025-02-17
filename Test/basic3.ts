7dfc58993bb0064bf2fa7964a272eb912ab936a6
Lovekesh <99822004+Loveqauser@users.noreply.github.com>
2025-02-17 16:32:20 -0500

    Create basic3.ts via website

---
+++ Test/basic3.ts
@@ -0,0 +1,17 @@
+export{}
+let name = 'Lovekesh Sharma - new file from website'
+console.log(name);
+console.log(`Changed name is: ${name}`);
+
+let num1:number = 10;
+console.log(num1);
+let str: string = 'TypeScript' ;
+console.log(str);
+let learningTypeScript: boolean = true 
+console.log(learningTypeScript);
+let undef:undefined;
+console.log(undef);
+let undef1;
+console.log(undef1);
+let nulltest:null = null;
+console.log(nulltest);
